1ce39ba6db9ee6dd5bfa82111748e5f214ddabbd
Varad <none>
2026-04-04 11:57:27 +0530

    working rtos (round robin)

--- main.c
+++ main.c
@@ -2,80 +2,111 @@
 #include "device.h"
 #include "core_cm3.h"
 #include "tcb.h"
+#include "scheduler.h"
 
 void systick_init(uint32_t tick_hz);
-
 void init_task_stack(TCB_t *tcb, void (*task_func)(void));
+void task_delay(uint32_t ticks);
 
-// ----------- GPIO (PC13 LED) -----------
 #define RCC_APB2ENR   (*(volatile uint32_t*)0x40021018)
 #define GPIOC_CRH     (*(volatile uint32_t*)0x40011004)
 #define GPIOC_ODR     (*(volatile uint32_t*)0x4001100C)
 
 void gpio_init(void) {
-    // Enable GPIOC clock
     RCC_APB2ENR |= (1 << 4);
-
-    // Configure PC13 as output (2 MHz push-pull)
     GPIOC_CRH &= ~(0xF << 20);
     GPIOC_CRH |=  (0x2 << 20);
 }
 
-// ----------- Delay -----------
-void delay(volatile uint32_t count) {
-    while (count--) {
-        __asm volatile("nop");
-    }
-}
-
-// ----------- Globals -----------
 uint32_t task1_stack[128];
-TCB_t tcb1;
+uint32_t task2_stack[128];
+uint32_t task3_stack[128];
+uint32_t idle_stack[64];
+TCB_t tcb1, tcb2, tcb3, tcb_idle;
 
-// ----------- Task -----------
+// Toggles LED every 500ms
 void task1(void) {
     while (1) {
-        GPIOC_ODR ^= (1 << 13); 
-        delay(500000);
+        GPIOC_ODR ^= (1 << 13);
+        task_delay(500);
     }
 }
 
-// ----------- Main -----------
+// Increments a counter every 200ms
+volatile uint32_t task2_count = 0;
+void task2(void) {
+    while (1) {
+        task2_count++;
+        task_delay(200);
+    }
+}
+
+// Increments a counter every 300ms
+volatile uint32_t task3_count = 0;
+void task3(void) {
+    while (1) {
+        task3_count++;
+        task_delay(300);
+    }
+}
+
+void idle_task(void) {
+    while(1);
+}
+
 int main(void) {
 
     systick_init(1000);
-    gpio_init();   // init LED
+    gpio_init();
 
-    // Setup TCB
     tcb1.stack_base = task1_stack;
     tcb1.stack_size = 128;
-    tcb1.priority = 0;
-    tcb1.state = TASK_READY;
+    tcb1.priority   = 0;
+    tcb1.state      = TASK_READY;
     tcb1.delay_ticks = 0;
-
-    // Initialize task stack
     init_task_stack(&tcb1, task1);
+    scheduler_add_task(&tcb1);
 
-    // Start first task (manual context restore)
+    tcb2.stack_base = task2_stack;
+    tcb2.stack_size = 128;
+    tcb2.priority   = 0;
+    tcb2.state      = TASK_READY;
+    tcb2.delay_ticks = 0;
+    init_task_stack(&tcb2, task2);
+    scheduler_add_task(&tcb2);
+
+    tcb3.stack_base = task3_stack;
+    tcb3.stack_size = 128;
+    tcb3.priority   = 0;
+    tcb3.state      = TASK_READY;
+    tcb3.delay_ticks = 0;
+    init_task_stack(&tcb3, task3);
+    scheduler_add_task(&tcb3);
+
+    tcb_idle.stack_base = idle_stack;
+    tcb_idle.stack_size = 64;
+    tcb_idle.priority   = 0;
+    tcb_idle.state      = TASK_READY;
+    tcb_idle.delay_ticks = 0;
+    init_task_stack(&tcb_idle, idle_task);
+    scheduler_add_task(&tcb_idle);
+
+    TCB_t *first = scheduler_get_current();
+
     __asm volatile(
-        "MOV R0, %0       \n"   // R0 = tcb1.sp
+        "MOV R0, %0       \n"
         "MSR PSP, R0      \n"
-
         "MOV R0, #2       \n"
         "MSR CONTROL, R0  \n"
         "ISB              \n"
-
-        // Restore full context
         "POP {R4-R11}     \n"
         "POP {R0-R3}      \n"
         "POP {R12}        \n"
         "POP {LR}         \n"
         "POP {PC}         \n"
-
         :
-        : "r"(tcb1.sp)
+        : "r"(first->sp)
     );
 
-    // Should never reach here
-    while (1);
+    while(1);
 }
--- rtos/systick.c
+++ rtos/systick.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include "device.h"
 #include "core_cm3.h"
+#include "scheduler.h"
 
+#define GPIOC_ODR (*(volatile uint32_t*)0x4001100C)
 
 volatile uint32_t tick_count = 0;
 
@@ -10,10 +12,12 @@ void systick_init(uint32_t tick_hz){
     SysTick->LOAD = (SYSCLK_HZ/tick_hz) -1;
     SysTick->VAL = 0;
     SysTick->CTRL =  SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+    NVIC_SetPriority(PendSV_IRQn, 0xFF); // setting pendsv to lowest priority , shouldnt interfere with any other interrupts
 }
 
 void SysTick_Handler(){
     tick_count++;
+    scheduler_tick();
+    SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
 
-    //TODO pendsv stuff
 }
